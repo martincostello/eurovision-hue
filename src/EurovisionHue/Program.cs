ceebcd1eeee09616e52132bd1d24624de3c8a274
martincostello <martin@martincostello.com>
2025-06-23 19:15:36 +0100

    Add Dockerfile
    Add a Dockerfile to build the app for easy running by people from just cloning the repo.

--- src/EurovisionHue/Program.cs
+++ src/EurovisionHue/Program.cs
@@ -1,6 +1,11 @@
 ﻿// Copyright (c) Martin Costello, 2025. All rights reserved.
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
+if (args.SequenceEqual(["--install-deps"]))
+{
+    return Microsoft.Playwright.Program.Main(["install", "chromium", "--with-deps"]);
+}
+
 using var cts = new CancellationTokenSource();
 
 Console.CancelKeyPress += (_, e) =>
